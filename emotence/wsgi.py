dcbd561ef78173816f9b2c1340a45b437bde8b87
sorXCode <sorXCode@gmail.com>
2019-07-30 02:25:48 +0100

    configure for ibm

--- emotence/wsgi.py
+++ emotence/wsgi.py
@@ -9,8 +9,18 @@ https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 
 import os
 
+# from django.core.wsgi import get_wsgi_application
+
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emotence.settings')
+
+# application = get_wsgi_application()
+
+import os
+
 from django.core.wsgi import get_wsgi_application
+from dj_static import Cling
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'emotence.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "emotence.settings")
 
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
+# application = get_wsgi_application()
