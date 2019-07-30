16cab479f431ce506aefa794f10e9702c93daad9
Victor Adeyanju <32789220+sorXCode@users.noreply.github.com>
2019-07-30 12:47:13 +0100

    Login feat (#2)
    * init user management
    
    * init user managmt
    
    * restore point 1

--- feeds/urls.py
+++ feeds/urls.py
@@ -1,6 +1,9 @@
-from django.urls import path
+# from django.contrib import admin
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
     path("", views.HomePageView.as_view(), name="home"),
+    path("accounts/", include('django.contrib.auth.urls')),
+    path('accounts/', include('accounts.urls')),
 ]
