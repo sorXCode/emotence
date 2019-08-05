7bc61d669e901b6879e1fa0680dfd91dff5469fd
sorXCode <sorXCode@gmail.com>
2019-08-05 10:25:54 +0100

    completed login/signup feature

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,10 @@
-from django.contrib.auth.forms import UserCreationForm
+# from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
+from .forms import CustomUserCreationForm
 
 
 class SignUpView(generic.CreateView):
-    form_class = UserCreationForm
-    success_url = reverse_lazy('home')
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
     template_name = 'signup.html'
