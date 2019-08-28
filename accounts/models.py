1963842bb361aab672ab7198c008cb1e44efc9e4
sorXCode <sorxcode@gmail.com>
2019-08-28 19:54:12 +0100

    form completed

---
+++ accounts/models.py
@@ -0,0 +1,14 @@
+# from django.db import models
+from django.contrib.auth.models import User
+
+
+class UserModel(User):
+    """
+    User model for accounts
+    """
+
+    def __str__(self):
+        return self.username
+
+    # def get_absolute_url(self):
+    #     return reverse("userbase_detail", kwargs={"pk": self.pk})
